ba9a390a6e9a09813e674d5ac397671bb5d166b6
Hisyam Maulana <hisyammaulana100@gmail.com>
2025-05-24 11:07:36 +0700

    menu active

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sidebar-brand-text mx-3">SICUREM <sup>ADMIN</sup></div>
     </a>
     <hr class="sidebar-divider my-0">
-    <li class="nav-item active">
+    <li class="nav-item {{ Request::is('dashboard*') ? 'active' : ''}}">
         <a class="nav-link" href="{{ route('dashboard') }}">
             <i class="fas fa-fw fa-home"></i>
             <span>Dashboard</span></a>
@@ -14,49 +14,49 @@
     </div>
     @if (Auth::guard('web')->check() && Auth::guard('web')->user()->role == 'admin')
     {{-- @if (Auth::user()->role == 'admin') --}}
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('ticket-customer*') ? 'active' : ''}}">
         <a class="nav-link" href="{{ route('ticket-customer.index') }}">
             <i class="fas fa-fw fa-flag"></i>
             <span>Keluhan / Laporan</span></a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('customer*') ? 'active' : ''}}">
         <a class="nav-link" href="{{ route('customer.index') }}">
             <i class="fas fa-fw fa-users"></i>
             <span>Pelanggan</span></a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ (Request::is('departement*') || Request::is('package*') || Request::is('user*')) ? 'active' : ''}}">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-cog"></i>
             <span>Master</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="collapseTwo" class="collapse {{ (Request::is('departement*') || Request::is('package*') || Request::is('user*')) ? 'show' : ''}}" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Pilihan Menu :</h6>
-                <a class="collapse-item" href="{{ route('departement.index') }}">Divisi</a>
-                <a class="collapse-item" href="{{ route('package.index') }}">Paket Layanan</a>
-                <a class="collapse-item" href="{{ route('user.index') }}">Manajemen User</a>
+                <a class="collapse-item {{ Request::is('departement*') ? 'active' : ''}}" href="{{ route('departement.index') }}">Divisi</a>
+                <a class="collapse-item {{ Request::is('package*') ? 'active' : '' }}" href="{{ route('package.index') }}">Paket Layanan</a>
+                <a class="collapse-item {{ Request::is('user*') ? 'active' : '' }}" href="{{ route('user.index') }}">Manajemen User</a>
             </div>
         </div>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('report*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('report.index') }}">
             <i class="fas fa-fw fa-file"></i>
             <span>Laporan</span></a>
     </li>
     @elseif (Auth::guard('web')->check() && Auth::guard('web')->user()->role == 'user')
     {{-- @elseif (Auth::user()->role == 'user') --}}
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('ticket-customer*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('ticket-customer.index') }}">
             <i class="fas fa-fw fa-flag"></i>
             <span>Keluhan / Laporan</span></a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('customer*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('customer.index') }}">
             <i class="fas fa-fw fa-users"></i>
             <span>Pelanggan</span></a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ Request::is('report*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('report.index') }}">
             <i class="fas fa-fw fa-file"></i>
             <span>Laporan</span></a>
